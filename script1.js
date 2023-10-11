cc34c0d422693b6f0675e83b75b35d46b76b1e64
LakshmipriyaNidamanuri <118598122+LakshmipriyaNidamanuri@users.noreply.github.com>
2023-10-11 18:24:30 +0530

    This is week1 Portfolio Project

---
+++ script1.js
@@ -0,0 +1,5 @@
+// You can add interactivity as needed. For instance:
+document.addEventListener("DOMContentLoaded", function() {
+    // Code to run after the document is loaded.
+    console.log("Website Loaded!");
+});
